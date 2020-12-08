041293a589b4450e396a7e444ac9fada45c6a65f
lunerien <luki90003@gmail.com>
2020-12-08 13:29:00 +0100

    Hardware

---
+++ hardware/servo_test.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(18,GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(18,1)
+        time.sleep(0.0025)
+        GPIO.output(18,0)
+
+        time.sleep(2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
